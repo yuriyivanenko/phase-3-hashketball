58f1dee6c4a2815aa49314d8992180997a8db058
Yuriy Ivanenko <yuriy.ivanenko@powerhrg.com>
2024-07-12 10:15:31 -0400

    Pass All Tests

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,6 @@
 # Write your code below game_hash
+require 'pry'
+
 def game_hash
   {
     home: {
@@ -126,4 +128,83 @@ def game_hash
   }
 end
 
-# Write code here
+def all_player_stats
+  all_players =  game_hash[:home][:players].concat(game_hash[:away][:players])
+end
+
+def player_stats(name)
+  result = all_player_stats.find { |player| player[:player_name] == name }
+end
+
+def num_points_scored(name)
+  player_stats(name)[:points]
+end
+
+def shoe_size(name)
+  player_stats(name)[:shoe]
+end
+
+def team_colors(team)
+  game_hash.each do |key, value|
+    if value[:team_name] == team
+      return value[:colors]
+    end
+  end
+end
+
+def team_names()
+  game_hash.map { |key, value| value[:team_name] }
+end
+
+def player_numbers(team)
+  team_data = game_hash.find { |key, value| value[:team_name] == team }
+  team_data.last[:players].map { |player| player[:number]}
+end
+
+def big_shoe_rebounds
+  biggest_shoe = {shoe: 0, rebounds: 0}
+  all_player_stats.each do |player|
+    if player[:shoe] > biggest_shoe[:shoe]
+      biggest_shoe = { shoe: player[:shoe], rebounds: player[:rebounds] }
+    end
+  end
+  biggest_shoe[:rebounds]
+end
+
+def most_points_scored
+  max_points = {player_name: "" , points: 0}
+  all_player_stats.each do |player|
+    if player[:points] > max_points[:points]
+      max_points = { player_name: player[:player_name], points: player[:points] }
+    end
+  end
+  max_points[:player_name]
+end
+
+def winning_team
+  home = game_hash[:home][:players].sum { |player|  player[:points] }
+  away = game_hash[:away][:players].sum { |player|  player[:points] }
+  home > away ? game_hash[:home][:team_name] : game_hash[:away][:team_name]
+end
+
+def player_with_longest_name
+  all_names = all_player_stats.map { |player| player[:player_name] }
+  longest_name = ""
+  all_names.each do |name|
+    if name.size > longest_name.size
+      longest_name = name
+    end
+  end
+  longest_name
+end
+
+def long_name_steals_a_ton
+  result = player_stats(player_with_longest_name)[:steals]
+  most_steals = 0
+  all_player_stats.each do |player|
+    if player[:steals] > result
+      most_steals = player[:steals]
+    end
+  end
+  result > most_steals ? true : false
+end
